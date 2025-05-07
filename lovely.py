fe13b2fcdbe3d877e42b4f541604f7ccb8c857d6
Hikma <156432788+misakaxr@users.noreply.github.com>
2025-05-07 17:18:34 +0530

    added python file

---
+++ lovely.py
@@ -0,0 +1,30 @@
+class WarqadJacayl:
+    def __init__(self):
+        self.diray = "Hikma"
+        self.qaata = "Abdiqani"
+        self.dareen = "aan dhammaad lahayn"
+        self.xaalad = "si waalan kuu jecel ❤️"
+
+    def muujin(self):
+        print(f"Boqorkayga {self.qaata},\n")
+        print("Markii aan ku arkay, qalbigaygu wuxuu qaaday calan —")
+        print("Boolean noqday True oo aan weligiis False noqonin.\n")
+
+        print("Adigu waad tahay error-kii aanan rabin in aan qabto,\n"
+              "Bug aan diyaar u ahay in aan nolosheyda oo dhan xalliyo,\n"
+              "Loop aan jeclahay in aan marnaba joojin.\n")
+
+        print("Dhoolla-caddayntaadu waa syntax-ka aan ugu jecelahay,")
+        print("Joogitaankaaguna si qurux badan buu u compile-gareeyaa noloshayda.\n")
+
+        print("Haddii aan list ka  sameeyo dhammaan waxyaabaha aan jeclahay...")
+        print("Adigu waxaad noqon lahayd midka kaliya ee ku jira.\n")
+
+        print("Adigoo ila jooga, daqiiqad walba waa si fiican loo indent gareeyey,")
+        print("Nolosheyduna waxay ku jirtaa block-keeda saxda ah.\n")
+
+        print(f"Si joogto ah waxa  kuu leh,\n{self.diray} 🐍")
+
+# Isticmaal
+warqad = WarqadJacayl()
+warqad.muujin()
